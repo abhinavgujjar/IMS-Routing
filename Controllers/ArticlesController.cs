0d0046636f17033ba3485463f8b7a7b4e0ed95cf
abhinav <abhinavgujjar@gmail.com>
2013-12-12 21:27:19 +0530

    added

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        //
+        // GET: /Articles/
+
+        public string Index()
+        {
+            return "Home";
+        }
+
+        //
+        // GET: /Articles/Details/5
+
+        public int Details(int id)
+        {
+            return id;
+        }
+
+        public string ByAuthor(string author)
+        {
+            if (author == "Scott")
+            {
+                return "A,B,C";
+            }
+
+
+            return "oops";
+        }
+
+        //
+        // GET: /Articles/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Articles/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Articles/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Articles/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Articles/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Articles/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/LiveScoresController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class LiveScoresController : Controller
+    {
+        //
+        // GET: /LiveScores/
+
+        public string Index()
+        {
+            return "Livescores index";
+        }
+
+        //
+        // GET: /LiveScores/Details/5
+
+        public string Details(string country, int over)
+        {
+            if (country == "India")
+            {
+                if (over == 40)
+                    return "234/6";
+            }
+
+            if (country == "Pakistan")
+            {
+                if (over == 20)
+                    return "35/7";
+            }
+
+            return "Aww.. snap";
+        }
+
+        //
+        // GET: /LiveScores/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /LiveScores/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /LiveScores/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /LiveScores/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /LiveScores/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /LiveScores/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
